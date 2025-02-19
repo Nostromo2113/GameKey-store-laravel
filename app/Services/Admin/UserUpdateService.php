3f5263e37f2e38acde80e998910ac367308b08b7
Yaroslav-Yurev <babaii323@yandex.ru>
2025-02-19 13:53:16 +0300

    UserUpdateService created. The logic from the controller was transferred to this class and decomposition was carried out. UpdateRequest fixed

---
+++ app/Services/Admin/UserUpdateService.php
@@ -0,0 +1,92 @@
+<?php
+
+namespace App\Services\Admin;
+
+use App\Models\User;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Storage;
+
+class UserUpdateService
+{
+    /**
+     * Обновляет данные пользователя и, при необходимости, его аватар.
+     *
+     * @param array $data Данные для обновления пользователя.
+     * @param User $user Пользователь, которого нужно обновить.
+     * @return User Обновленный пользователь.
+     * @throws Exception Если произошла ошибка при обновлении.
+     */
+    public function update(array $data, User $user): User
+    {
+        DB::beginTransaction();
+        try {
+
+            $imagePath = $this->updateAvatar($user, $data);
+
+            $this->fillUser($user, $data, $imagePath);
+
+            DB::commit();
+
+            return $user;
+
+        } catch (\Exception $e) {
+            DB::rollBack();
+            throw $e;
+        }
+    }
+
+    /**
+     * Заполняет данные пользователя.
+     *
+     * @param User $user Пользователь, которого нужно обновить.
+     * @param array $data Данные для обновления.
+     * @param string $imagePath Путь к аватару пользователя.
+     * @return void
+     */
+    private function fillUser(User $user, array $data, string $imagePath): void
+    {
+        $user->fill([
+            'name' => $data['name'],
+            'email' => $data['email'],
+            'phone_number' => $data['phone_number'],
+            'surname' => $data['surname'],
+            'patronymic' => $data['patronymic'],
+            'age' => $data['age'],
+            'address' => $data['address'],
+            'avatar' => $imagePath,
+        ])->save();
+    }
+
+    /**
+     * Обновляет аватар пользователя.
+     *
+     * @param User $user Пользователь, у которого обновляется аватар.
+     * @param array $data Данные для обновления, включая файл аватара (если есть).
+     * @return string Путь к новому или старому аватару.
+     */
+    private function updateAvatar(User $user, array $data): string
+    {
+        $oldImagePath = $user->avatar;
+
+        try {
+            // Если аватар был отправлен
+            if (isset($data['file'])) {
+                // Сохраняем новый аватар
+                $imagePath = Storage::disk('public')->put('uploads/users/avatars', $data['file']);
+
+                // Удаляем старый аватар, если он не является аватаром по умолчанию
+                if ($oldImagePath != 'uploads/users/avatars/default_avatar.jpg' && Storage::disk('public')->exists($oldImagePath)) {
+                    Storage::disk('public')->delete($oldImagePath);
+                }
+            } else {
+                // Если новый аватар не отправлен, оставляем старый
+                $imagePath = $oldImagePath;
+            }
+
+            return $imagePath;
+
+        } catch (\Exception $e) {
+            throw $e;
+        }
+    }
+}

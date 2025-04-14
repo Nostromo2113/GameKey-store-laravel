e9f38aedee685523b806501985800c203ae559e6
Yaroslav-Yurev <arklaid11@gmail.com>
2025-04-14 18:53:27 +0300

    refactor ActivationKeyGenerator

--- app/Services/Admin/ActivationKeyGeneratorService.php
+++ app/Services/Admin/ActivationKeyGeneratorService.php
@@ -7,10 +7,27 @@ use Illuminate\Support\Str;
 
 class ActivationKeyGeneratorService
 {
-    public function generate(): string
+    public function generateForProduct(int $productId, int $quantity): void
+    {
+        $keys = [];
+        for ($i = 0; $i < $quantity; $i++) {
+            $keys[] = [
+                'product_id' => $productId,
+                'key' => $this->generate(),
+                'created_at' => now(),
+            ];
+        }
+        ActivationKey::insert($keys);
+    }
+
+    private function generate(): string
     {
         do {
-            $key = strtoupper(Str::random(5)) . '-' . strtoupper(Str::random(5)) . '-' . strtoupper(Str::random(5));
+            $key = strtoupper(
+                Str::random(5) . '-' .
+                Str::random(5) . '-' .
+                Str::random(5)
+            );
         } while (ActivationKey::where('key', $key)->exists());
 
         return $key;

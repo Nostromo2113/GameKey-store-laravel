b212b85d90ad5a0015e9ace9f8fa22ca16338b84
Yaroslav-Yurev <arklaid11@gmail.com>
2025-04-23 20:24:27 +0300

    removed redundant try catch in UserDestroyer

--- app/Services/Admin/User/UserDestroyer.php
+++ app/Services/Admin/User/UserDestroyer.php
@@ -9,23 +9,15 @@ class UserDestroyer
 {
     public function destroyUser(User $user): void
     {
-        try {
             $this->deleteUserAvatar($user);
             $user->delete();
-        } catch (\Exception $e) {
-            throw new \Exception('Ошибка при удалении пользователя: ' . $e->getMessage());
-        }
     }
 
     private function deleteUserAvatar(User $user): void
     {
-        try {
             $avatar = $user->avatar;
             if ($avatar != 'uploads/users/avatars/default_avatar.jpg' && Storage::disk('public')->exists($avatar) && isset($data['file'])) {
                 Storage::disk('public')->delete($avatar);
             }
-        } catch (\Exception $e) {
-            throw new \Exception('Ошибка при удалении аватара пользователя: ' . $e->getMessage());
-        }
     }
 }
